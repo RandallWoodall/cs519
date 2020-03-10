6e4fdce70e81ec99796163b22d542ce0cba80d44
Ayatelrahman Elsayed <aeit.el.rahman.nasser.k@gmail.com>
2020-03-10 01:57:04 -0700

    data_files
    data_files

---
+++ proj/stage3/weather_gather_oldData.py
@@ -0,0 +1,52 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import time
+import datetime
+hourly_weather = [] #list of weather
+import numpy as np
+
+
+#historical
+
+day = np.array ([20200207,20200208,20200209,20200206,20200205,20200204])
+
+for i in range(day.size):
+
+    old_data = requests.get ('https://api.weather.com/v2/pws/history/all?stationId=KNMLASCR263&format=json&units=e&date='+str(day[i])+'&apiKey=33b111b523f94b80b111b523f9bb80b8', auth=HTTPBasicAuth('KNMLASCR263', 'N2miGFcR'))
+    #forecast daily/5 days
+
+    future_data = requests.get ('https://api.weather.com/v3/wx/forecast/daily/5day?geocode=32.28123,-106.753756&format=json&units=e&language=en-US&apiKey=33b111b523f94b80b111b523f9bb80b8')
+    print(future_data)
+
+    print(old_data.status_code)
+
+    print(old_data.json())
+    dict_ = old_data.json()
+    output = open('solarData' + str(day[i])+ '.csv', 'w')
+    for hour in dict_['observations']:
+        output.write(hour['obsTimeLocal'] + ',' + str(hour['solarRadiationHigh']) + ',' + str(hour['uvHigh']) + ',' + str(
+            hour['winddirAvg']) + ',' + str(hour['humidityHigh']) + ',' + str(hour['humidityLow']) + ',' + str(
+            hour['humidityAvg']) + ',' + str(hour['qcStatus'])
+                + ',' + str(hour['imperial']['tempHigh']) + ',' + str(hour['imperial']['tempLow']) + ',' + str(
+            hour['imperial']['tempAvg']) + ',' + str(hour['imperial']['windspeedHigh']) + ',' + str(
+            hour['imperial']['windgustLow']) + ',' + str(hour['imperial']['windspeedAvg'])
+                + ',' + str(hour['imperial']['dewptHigh']) + ',' + str(hour['imperial']['dewptLow']) + ',' + str(
+            hour['imperial']['dewptAvg']) + ',' + str(hour['imperial']['windchillHigh']) + ',' + str(
+            hour['imperial']['windchillAvg']) + ',' + str(hour['imperial']['heatindexHigh'])
+                + ',' + str(hour['imperial']['heatindexLow']) + ',' + str(hour['imperial']['heatindexAvg']) + ',' + str(
+            hour['imperial']['pressureMax']) + ',' + str(hour['imperial']['pressureMin']) + ',' + str(
+            hour['imperial']['pressureTrend']) + ',' + str(hour['imperial']['precipRate'])
+                + ',' + str(hour['imperial']['precipTotal']) + '\n')
+    output.close()
+
+
+    def jprint(obj):
+        # create a formatted string of the Python JSON object
+        text = json.dumps(obj, sort_keys=False, indent=4)
+        print(text)
+
+
+    jprint(future_data.json())
+
+  
