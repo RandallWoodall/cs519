98c26ad6905ee70b9ceada63e0e29f6c9a063a6a
Ayatelrahman Elsayed <aeit.el.rahman.nasser.k@gmail.com>
2020-04-02 18:47:41 -0600

    uptaded data with headers

--- proj/stage3/Merging_data.py
+++ proj/stage3/Merging_data.py
@@ -4,19 +4,24 @@ import pandas as pd
 list_files = os.listdir("weather_data")
 print(list_files)
 data = open('Data' + '.csv', 'w')
+H_flag = 1 #flag for puttingg header one time
 for py in list_files:
     date = (py[-8:])
     files = glob.glob('*'+ str(date))
     print(files)
-    col_Names=["time", "1", "2", "3", "4", "5", "6", "7", "8", "9","10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"
-    ,"22", "23", "24", "25", "26"]
+    col_Names=["time", "solarRadiation", "uvHigh", "winddirAvg", "humidityHigh", "humidityLow", "humidityAvg", "qcStatus", "tempHigh", "tempLow","tempAvg", "windspeedHigh", "windgustLow", "windspeedAvg", "dewptHigh", "dewptLow", "dewptAvg", "windchillHigh", "windchillAvg", "heatindexHigh"
+    ,"heatindexLow", "heatindexAvg", "pressureMax", "pressureMin", "pressureTrend","precipRate","precipTotal"]
     weather = pd.read_csv(files[0],names= dict.fromkeys(col_Names , 1))
     weather_temp = (weather.iloc[:,0]).str.split(expand=True) 
     weather = pd.concat([weather_temp.iloc[:,1],weather.iloc[:,1:] ],axis=1,names= col_Names)
     weather.columns = col_Names
     power = pd.read_csv(files[1],header=None,names=["time","power"])
     result = pd.merge(weather, power, on='time', how='inner')
-    result.to_csv("Data.csv",index=False, mode='a')
+    if H_flag == 1:
+        result.to_csv("Data.csv",index=False, mode='a')
+    else:
+        result.to_csv("Data.csv",index=False, mode='a',header=None)
+    H_flag = 0
     #print(files)
 
 #time_p =  (power.iloc[:,0:2])
