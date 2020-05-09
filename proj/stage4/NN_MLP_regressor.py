9c8c4caf5ff38044cd34dbf5f1c6d23f0ded8999
Randall Woodall <randalljwoodall@gmail.com>
2020-05-08 23:28:45 -0600

    changing mse to rmse

--- proj/stage4/NN_MLP_regressor.py
+++ proj/stage4/NN_MLP_regressor.py
@@ -9,7 +9,8 @@ Random forest.
 
 import numpy as np
 import matplotlib.pyplot as plt 
-import pandas as pd  
+import pandas as pd
+from math import sqrt
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error#RMSE
@@ -45,9 +46,9 @@ print('r2 score for train data: ' + str(r2))
 r2 = r2_score(y_true=y_test, y_pred=y_test_pred)
 print('r2 score for test data: ' + str(r2))
 
-RMSE = mean_squared_error(y_true=y_train, y_pred=y_train_pred)
+RMSE = sqrt(mean_squared_error(y_true=y_train, y_pred=y_train_pred))
 print('RMSE score for train data: ' + str(RMSE))
-RMSE = mean_squared_error(y_true=y_test, y_pred=y_test_pred)
+RMSE = sqrt(mean_squared_error(y_true=y_test, y_pred=y_test_pred))
 print('RMSE score for test data: ' + str(RMSE))
 
 MAE = mean_absolute_error(y_true=y_train, y_pred=y_train_pred)
